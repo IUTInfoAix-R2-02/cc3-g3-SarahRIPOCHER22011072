69a165badb5156b369adf7535e421b974340f439
Sarah <sarah.ripoche@etu.univ-amu.fr>
2023-06-02 15:28:39 +0200

    partie 2 creation des cercles en fonction des axes

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -6,6 +6,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -30,6 +33,18 @@ public class ToileController implements Initializable {
     @FXML
     private TextField comp6;
 
+    @FXML
+    private HBox scene;
+
+    @FXML
+    private Pane spider;
+
+    private Circle c1;
+    private Circle c2;
+    private Circle c3;
+    private Circle c4;
+    private Circle c5;
+    private Circle c6;
 
 
 
@@ -38,14 +53,62 @@ public class ToileController implements Initializable {
     }
 
     @FXML
-    protected void handleCompAction(ActionEvent event){
+    protected void handleCompAction(ActionEvent event) {
         TextField source = (TextField) event.getSource();
+        int note = 0;
         try {
-            int nb = Integer.parseInt(source.getText());
-            System.out.println(nb);
-        } catch (NumberFormatException e){
+            note = Integer.parseInt(source.getText());
+            System.out.println(note);
+        } catch (NumberFormatException e) {
             System.out.println("Probleme de saisie du nombre");
         }
+        // Calcul des coordonées du centre du cercle en fonction de l'axe
+        int x = 0;
+        int y = 0;
+        if (source.getId() == comp1.getId()){
+            x = getXRadarChart(note, 1);
+            y = getYRadarChart(note, 1);
+            // Dessin du cercle
+            spider.getChildren().remove(c1);    // Enlever un point deja present
+            c1 = new Circle( x,y, 5);
+            spider.getChildren().add(c1);
+        } else if (source.getId() == comp2.getId()){
+            x = getXRadarChart(note, 2);
+            y = getYRadarChart(note, 2);
+            // Dessin du cercle
+            spider.getChildren().remove(c2);    // Enlever un point deja present
+            c2 = new Circle( x,y, 5);
+            spider.getChildren().add(c2);
+        } else if (source.getId() == comp3.getId()){
+            x = getXRadarChart(note, 3);
+            y = getYRadarChart(note, 3);
+            // Dessin du cercle
+            spider.getChildren().remove(c3);    // Enlever un point deja present
+            c3 = new Circle( x,y, 5);
+            spider.getChildren().add(c3);
+        } else if (source.getId() == comp4.getId()){
+            x = getXRadarChart(note, 4);
+            y = getYRadarChart(note, 4);
+            // Dessin du cercle
+            spider.getChildren().remove(c4);    // Enlever un point deja present
+            c4 = new Circle( x,y, 5);
+            spider.getChildren().add(c4);
+        } else if (source.getId() == comp5.getId()){
+            x = getXRadarChart(note, 5);
+            y = getYRadarChart(note, 5);
+            // Dessin du cercle
+            spider.getChildren().remove(c5);    // Enlever un point deja present
+            c5 = new Circle( x,y, 5);
+            spider.getChildren().add(c5);
+        } else if (source.getId() == comp6.getId()){
+            x = getXRadarChart(note, 6);
+            y = getYRadarChart(note, 6);
+            // Dessin du cercle
+            spider.getChildren().remove(c6);    // Enlever un point deja present
+            c6 = new Circle( x,y, 5);
+            spider.getChildren().add(c6);
+        }
+
 
     }
 
