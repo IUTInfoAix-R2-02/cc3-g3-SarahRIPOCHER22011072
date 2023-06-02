8af22615cf7d906fe63ec5e4d1fce7511f32809e
Sarah <sarah.ripoche@etu.univ-amu.fr>
2023-06-02 14:50:12 +0200

    partie 2 detection des nombres entrés dans les textfields

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -1,27 +1,13 @@
 package fr.amu.iut.cc3;
 
-import javafx.beans.binding.Bindings;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleListProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.shape.Line;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class ToileController implements Initializable {
@@ -31,9 +17,36 @@ public class ToileController implements Initializable {
     private static int angleDepart = 90;
     private static int noteMaximale = 20;
 
+    @FXML
+    private TextField comp1;
+    @FXML
+    private TextField comp2;
+    @FXML
+    private TextField comp3;
+    @FXML
+    private TextField comp4;
+    @FXML
+    private TextField comp5;
+    @FXML
+    private TextField comp6;
+
+
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    @FXML
+    protected void handleCompAction(ActionEvent event){
+        TextField source = (TextField) event.getSource();
+        try {
+            int nb = Integer.parseInt(source.getText());
+            System.out.println(nb);
+        } catch (NumberFormatException e){
+            System.out.println("Probleme de saisie du nombre");
+        }
+
     }
 
     int getXRadarChart(double value, int axe ){
